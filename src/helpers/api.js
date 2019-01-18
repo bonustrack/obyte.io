b3c05aab6888b1401be7d3fc5eacb5c5f2b753fe
adcpm <fabien@bonustrack.co>
2019-01-18 20:04:34 +0700

    Change WS endpoint

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ const kbyte = require('kbyte');
 const Promise = require('bluebird');
 
 Promise.promisifyAll(kbyte.Client.prototype);
-const api = new kbyte.Client('wss://byteball.herokuapp.com');
+const api = new kbyte.Client('wss://obyte.herokuapp.com');
 // const api = new kbyte.Client('ws://localhost:5000');
 
 export default api;
