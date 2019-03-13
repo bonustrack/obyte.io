f2c326bee52dd14923c4e5278c282b3faf32b22f
Tarmo Annus <tarmo888@hotmail.com>
2019-03-13 19:45:32 +0200

    unified JSON style

--- src/components/Message/Asset.vue
+++ src/components/Message/Asset.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>Issue {{$n(message.payload.cap)}} {{message.unit}}</p>
+  <div class="w-100">
+    <p>Issued {{$n(message.payload.cap)}} {{message.unit}}</p>
+    <ul class="Box Box--condensed d-inline-block w-100">
+      <li class="Box-row" v-if="field!=='cap'" v-for="field in Object.keys(message.payload)">
+        <p class="m-0">
+          <span class="text-bold">{{field}}:</span>
+          {{message.payload[field]}}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
